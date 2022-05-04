41901dfacd7559d6a170f69eb1338c5361896194
KarinaMashkanova <karyna.mashkanava@gmail.com>
2022-05-05 00:59:39 +0200

    removed unnecessary files

--- src/test/java/utilities/WebTimeouts.java
+++
@@ -1,8 +0,0 @@
-package utilities;
-
-public class WebTimeouts {
-
-    public static int PAGE_LOAD_TIMEOUT = 15;
-    public static int SCRIPT_TIMEOUT = 15;
-    public static int IMPLICIT_TIMEOUT = 15;
-}
